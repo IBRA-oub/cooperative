00b94e39fe06f3f8155f689dde8b6bfd3a5d185c
IBRA-oub <brahimbourih@gmail.com>
2024-03-19 13:52:25 +0000

    home page ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,73 @@
-import './bootstrap';
+import "./bootstrap";
+
+// --------------------chartjs---------------------
+
+const ctx = document.getElementById("barchart");
+
+new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
+
+const doughnut = document.getElementById("doughnut");
+
+new Chart(doughnut, {
+    type: "doughnut",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
+
+
+const line = document.getElementById("line");
+
+new Chart(line, {
+    type: "line",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
